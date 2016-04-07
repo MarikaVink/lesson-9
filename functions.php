73e69cc85d1917aa9927159c672087f38d35f166
marvin <marvin@greeny.cs.tlu.ee>
2016-04-07 13:10:28 +0300

    added restricted.php

--- functions.php
+++ functions.php
@@ -2,6 +2,52 @@
 
 	require_once("../../config.php");
 	
+	//start server session to store data
+	//in every file you want to taccess session
+	//you should reewquire functions
+	session_start();
+	
+	
+	function login($user, $pass){
+		//hash the pass
+		$pass = hash("sha512", $pass);
+		
+		//GLOBALS - access outsde variable in function
+		$mysql = new mysqli("localhost",$GLOBALS["db_username"], $GLOBALS["db_password"],"webpr2016_marvin");
+		
+		
+		
+		$stmt = $mysql->prepare("SELECT id FROM users WHERE username=? and password=?");
+		
+		echo $mysql->error;
+		
+		$stmt->bind_param("ss", $user, $pass);
+		
+		$stmt->bind_result($id);
+		
+		$stmt->execute();
+		
+		//get the data
+		if($stmt->fetch()){
+				echo "user with id".$id." logged in!";
+				
+			//create session variables
+			//redirect user
+			$_SESSION["user_id"] = $id;
+			$_SESSION["username"] = $user;
+			
+			header("Location: restrict.php");
+			
+			
+				
+		}else{
+			echo "wrong credentials";
+		}
+		
+		
+		
+	}
+	
 	function signup($user, $pass){
 		
 		//hash the password
--- login.php
+++ login.php
@@ -1,15 +1,40 @@
 <?php
 	require_once("functions.php");
-
+	
+	if(isset($_SESSION["user_id"])){
+		//redirect user to restricted page
+			header("Location: restrict.php");
+	}
+	
+		//login button clicked
 		if (isset($_POST["login"])){
 			
 			//login
 			
+			
 			echo "logging in ...";
 			
+			
+			//the fields are not empty
+			if(!empty($_POST["username"]) && !empty($_POST["password"])){
+
+		//save to db
+		
+		login($_POST["username"],$_POST["password"]);
+		
+		
+		}else{
+			
+			echo "both fields are required!";
+		}
+			
+		}
 			//signup button clicked
 			
-		}else if(isset($_POST["signup"])){
+		 
+		 
+		 
+		 if(isset($_POST["signup"])){
 			
 			//signup
 			
@@ -18,24 +43,24 @@
 			//the fields are not empty
 			if(!empty($_POST["username"]) && !empty($_POST["password"])){
 
-		//save to db
-		
-		signup($_POST["username"],$_POST["password"]);
-		
-		
-		}else{
+			//save to db
 			
-			echo "both fields are required!";
-		}
-		
-	}
+			signup($_POST["username"],$_POST["password"]);
+			
+			
+			}else{
+				
+				echo "both fields are required!";
+			}
+		 }
+	
 		
 	
 		
 ?>
 
 <h1>Log in</h1>
-<form>
+<form method="POST">
 
 	<input type="text"  placeholder="username" name="username">
 	<input type="password" placeholder="password" name="password">
