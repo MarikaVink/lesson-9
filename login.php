14e934e8f20f3a85e92b9de917e24a5b16c882b8
marvin <marvin@greeny.cs.tlu.ee>
2016-04-07 10:45:46 +0300

    created file login.php

---
+++ login.php
@@ -0,0 +1,19 @@
+<h1>Log in</h1>
+<form>
+
+	<input type="text"  placeholder="username" name="username">
+	<input type="text" placeholder="password" name="username">
+	
+	<input type="submit" name="login" value="Log in"
+	
+</form>
+
+<h1>Sign up</h1>
+<form>
+
+	<input type="text"  placeholder="username" name="username">
+	<input type="text" placeholder="password" name="username">
+	
+	<input type="submit" name="signup" value="Sign up"
+	
+</form>
